6a44ea0cd1e643e53aefadce61fbb2f35e366df3
cuongle <cuongle.dev@gmail.com>
2020-03-03 01:42:57 +0700

    first commit

--- app/src/main/java/com/hstc/lengoccuong/chatapp/Fragments/UsersFragment.java
+++ app/src/main/java/com/hstc/lengoccuong/chatapp/Fragments/UsersFragment.java
@@ -9,9 +9,12 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.hstc.lengoccuong.chatapp.Adapter.UserAdapter;
 import com.hstc.lengoccuong.chatapp.Model.User;
@@ -33,6 +37,7 @@ public class UsersFragment extends Fragment {
 
     private UserAdapter userAdapter;
     private List<User> mUsers;
+    EditText search_users;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,7 +51,67 @@ public class UsersFragment extends Fragment {
         mUsers = new ArrayList<>();
 
         readUsers();
+        search_users = view.findViewById(R.id.search_users);
+        search_users.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                searchUsers(charSequence.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
         return view;
+    }
+
+    private void searchUsers(String s) {
+
+        final FirebaseUser fuser = FirebaseAuth.getInstance().getCurrentUser();
+        Query query = FirebaseDatabase.getInstance().getReference("Users").orderByChild("username")
+                .startAt(s)
+                .endAt(s+ "\uf8ff");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsers.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+
+                    User user = snapshot.getValue(User.class);
+
+                    assert user != null;
+                    assert  fuser  != null;
+
+                    if (!user.getId().equals(fuser.getUid())){
+                        mUsers.add(user);
+                    }
+
+
+                }
+
+                userAdapter = new UserAdapter(getContext(), mUsers ,false);
+                recyclerView.setAdapter(userAdapter);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private void readUsers() {
@@ -59,20 +124,26 @@ public class UsersFragment extends Fragment {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                mUsers.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    User user = snapshot.getValue(User.class);
+                if (search_users.getText().toString().equals("")){
 
-                    assert user != null;
-                    assert firebaseUser != null;
+                    mUsers.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        User user = snapshot.getValue(User.class);
 
-                    if (!user.getId().equals(firebaseUser.getUid())){
-                        mUsers.add(user);
+                        assert user != null;
+                        assert firebaseUser != null;
+
+                        if (!user.getId().equals(firebaseUser.getUid())){
+                            mUsers.add(user);
+                        }
                     }
+
+                    userAdapter = new UserAdapter(getContext(), mUsers,false);
+                    recyclerView.setAdapter(userAdapter);
+
                 }
 
-                userAdapter = new UserAdapter(getContext(), mUsers,false);
-                recyclerView.setAdapter(userAdapter);
+
 
             }
 
